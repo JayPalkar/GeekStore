a91fd34f18ce6739af11699e1beda9b681c4470e
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-02-16 16:41:55 +0530

    Settingup Repository

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to GeekStore");
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something went wrong!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
