79dc8ba0509bfe6714f5e0383edd18fb556b0018
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-02-16 20:29:52 +0530

    product schema and model completed

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    imageURL: {
+      type: [String],
+      require: true,
+    },
+    price: {
+      type: float,
+      require: true,
+    },
+    stock: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+    averageRating: {
+      type: Number,
+    },
+    sellerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      require: true,
+    },
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    stateOfProduct: {
+      type: String,
+      enum: ["new_product,", "trending_product", "most_ratted"],
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
