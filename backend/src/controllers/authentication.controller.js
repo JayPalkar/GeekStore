76b1c6dcceacdbe39dedfa8a877be08fbeb33a60
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-02-18 18:38:26 +0530

    login API completed

--- backend/src/controllers/authentication.controller.js
+++ backend/src/controllers/authentication.controller.js
@@ -7,7 +7,7 @@ export const register = async (req, res) => {
   const { fullName, email, phone, password } = req.body;
   try {
     if (!fullName || !email || !phone || !password) {
-      return res.status(400).json({ message: "All fields are important" });
+      return res.status(400).json({ message: "All fields are required" });
     }
 
     if (password.length < 8) {
@@ -54,10 +54,35 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("login");
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await user.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullname: user.fullName,
+      email: user.email,
+      phone: user.phone,
+      role: user.role,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
-export const logout = (req, res) => {
-  res.send("logout");
-};
+export const logout = async (req, res) => {};
