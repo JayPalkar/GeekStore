ec65e2fa7c1a81ecbd95865394de8c7ca208b00d
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-02-18 18:26:38 +0530

    register API completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 import { connectToDB } from "./config/db.js";
+import authenticationRoutes from "./routes/authentication.routes.js";
 
 dotenv.config();
 
@@ -10,9 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to GeekStore");
-});
+app.use("/api/auth", authenticationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
