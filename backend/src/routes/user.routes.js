3f3f637fcfb2db661cc8b4bd76493afdfbfc630d
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-02-23 12:57:32 +0530

    added protectRoute

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
+import { protectRoute } from "../middlewares/authentication.middleware";
 
 const router = express.Router();
 
-router.get("/", authenticationMiddleware, getUserProfile);
-router.put("/", authenticationMiddleware, updateUserProfile);
-router.put("/become-a-seller", authenticationMiddleware, becomeASeller);
-router.delete("/", authenticationMiddleware, deleteUserProfile);
+router.get("/", protectRoute, getUserProfile);
+router.put("/", protectRoute, updateUserProfile);
+router.put("/become-a-seller", protectRoute, becomeASeller);
+router.delete("/", protectRoute, deleteUserProfile);
 
 export default router;
