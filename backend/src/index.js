3679a0b0e121d622b48d7d6ad01c8bdccd8eb3c2
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-05-29 18:31:29 +0530

    category apis completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 
 import { connectToDB } from "./config/db.js";
 import authenticationRoutes from "./routes/authentication.routes.js";
+import productRoutes from "./routes/product.routes.js";
+import categoryRoutes from "./routes/category.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
@@ -17,6 +19,7 @@ app.use(cookieParser());
 app.use("/api/auth", authenticationRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
