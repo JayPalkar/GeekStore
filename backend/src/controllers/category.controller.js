c47fe7948b1ababf1d468221edcb14d23f1b47db
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-05-29 17:37:26 +0530

    category routes setup completed

---
+++ backend/src/controllers/category.controller.js
@@ -0,0 +1,3 @@
+export const createCategory = async (req, res) => {};
+export const getAllCategories = async (req, res) => {};
+export const getProductsByCategory = async (req, res) => {};
---
+++ backend/src/routes/category.routes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  createCategory,
+  getAllCategories,
+  getProductsByCategory,
+} from "../controllers/category.controller";
+
+const router = express.Router();
+
+router.post("/", createCategory);
+router.get("/", getAllCategories);
+router.get("/:categoryId/products", getProductsByCategory);
