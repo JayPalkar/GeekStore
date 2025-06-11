8eeb527714732c094bbd265d0367ee8d6790e0b7
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-06-11 21:21:32 +0530

    products api completed (#17)
    * products api completed
    
    * made small changes

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -1,4 +1,6 @@
+import { truncates } from "bcryptjs";
 import Product from "../models/product.model.js";
+import User from "../models/user.model.js";
 
 export const createProduct = async (req, res) => {
   try {
@@ -46,8 +48,93 @@ export const createProduct = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
-export const getAllProducts = async (req, res) => {};
-export const getSingleProduct = async (req, res) => {};
-export const getMyProducts = async (req, res) => {};
-export const updateAProduct = async (req, res) => {};
-export const deleteAProduct = async (req, res) => {};
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    console.error("Error in getALlProducts controller ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const getSingleProduct = async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    console.error("Error in getALlProducts: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const getMyProducts = async (req, res) => {
+  try {
+    const loggedInUser = req.user._id;
+    const user = await User.findById(loggedInUser).select("role");
+
+    if (!user || user.role !== "seller") {
+      return res.status(404).json({
+        message:
+          "You are not a seller Update your profile to create and view your products",
+      });
+    }
+
+    const myProducts = await Product.find({ sellerId: loggedInUser });
+
+    if (myProducts.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No Products Found, Try creating a new product" });
+    }
+
+    res.status(200).json(myProducts);
+  } catch (error) {
+    console.error("Error in getMyProducts Controller: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const updateAProduct = async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const { name, description, imageURL, price, stock, stateOfProduct } =
+      req.body;
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      {
+        name,
+        description,
+        imageURL,
+        price,
+        stock,
+        stateOfProduct,
+      },
+      { new: true }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.error("Error in updateAProduct Controller: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+export const deleteAProduct = async (req, res) => {
+  try {
+    const { productId } = req.params;
+
+    const deletedProduct = await Product.findByIdAndDelete(productId);
+
+    res
+      .status(200)
+      .json({ message: "Product deleted successfully: ", deletedProduct });
+  } catch (error) {
+    console.error("Error in deleteAProduct Controller: ", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
