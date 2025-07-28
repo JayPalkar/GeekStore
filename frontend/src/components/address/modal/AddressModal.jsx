c271fad66ad8bafbb5665243d23c0472da63e585
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2025-07-28 13:26:30 +0530

    add address completed

--- frontend/src/components/address/modal/AddressModal.jsx
+++ frontend/src/components/address/modal/AddressModal.jsx
@@ -1,46 +1,88 @@
 import { useState } from "react";
 import "./addressModal.css";
+import { X } from "lucide-react";
+import { useUserStore } from "../../../store/useUserStore";
 
-const AddressModal = () => {
-  const [address, setAddress] = useState({
+const AddressModal = ({ closeModal }) => {
+  const [addressData, setAddressData] = useState({
     street: "",
     city: "",
     state: "",
     country: "",
     zip: "",
   });
+
+  const { addUserAddress, isUpdatingUser } = useUserStore();
+
+  const addNewAddress = () => {
+    addUserAddress({ address: addressData });
+    closeModal();
+  };
+
   return (
     <div className="addressModal">
+      <button
+        onClick={(e) => {
+          e.preventDefault;
+          closeModal();
+        }}
+        className="closeModalButton"
+      >
+        <X />
+      </button>
       <input
+        className="formInput"
         type="text"
         placeholder="Street"
-        value={address.street}
-        onChange={(e) => setAddress({ ...address, street: e.target.value })}
+        value={addressData.street}
+        onChange={(e) =>
+          setAddressData({ ...addressData, street: e.target.value })
+        }
       />
       <input
+        className="formInput"
         type="text"
         placeholder="City"
-        value={address.city}
-        onChange={(e) => setAddress({ ...address, city: e.target.value })}
+        value={addressData.city}
+        onChange={(e) =>
+          setAddressData({ ...addressData, city: e.target.value })
+        }
       />
       <input
+        className="formInput"
         type="text"
         placeholder="State"
-        value={address.State}
-        onChange={(e) => setAddress({ ...address, State: e.target.value })}
+        value={addressData.state}
+        onChange={(e) =>
+          setAddressData({ ...addressData, state: e.target.value })
+        }
       />
       <input
+        className="formInput"
         type="text"
         placeholder="Country"
-        value={address.country}
-        onChange={(e) => setAddress({ ...address, country: e.target.value })}
+        value={addressData.country}
+        onChange={(e) =>
+          setAddressData({ ...addressData, country: e.target.value })
+        }
       />
       <input
-        type="number"
+        className="formInput"
+        type="text"
+        inputMode={"numeric"}
+        pattern="\d{6}"
+        maxLength="6"
         placeholder="Postal Code"
-        value={address.zip}
-        onChange={(e) => setAddress({ ...address, zip: e.target.value })}
+        value={addressData.zip}
+        onChange={(e) =>
+          setAddressData({ ...addressData, zip: e.target.value })
+        }
+        onInput="this.value = this.value.replace(/[^0-9]/g, '').slice(0, 6)"
       />
+
+      <button onClick={addNewAddress} className="submitAddressFormButton">
+        {isUpdatingUser ? "Adding Address..." : "Add Address"}
+      </button>
     </div>
   );
 };
--- frontend/src/pages/address/address.jsx
+++ frontend/src/pages/address/address.jsx
@@ -3,25 +3,39 @@ import AddressBlock from "../../components/address/AddressBlock";
 import { useAuthenticationStore } from "../../store/useAuthenticationStore";
 import "./address.css";
 import AddressModal from "../../components/address/modal/AddressModal";
+import { useState } from "react";
 
 const Address = () => {
+  const [showModal, setShowModal] = useState(false);
+
   const { authUser } = useAuthenticationStore();
 
-  console.log(authUser.address);
+  const closeModal = () => {
+    setShowModal(false);
+    window.location.reload();
+  };
 
   return (
     <>
       <div className="addressContainer">
-        <button className="addressBlock addAddress">
+        <button
+          onClick={(e) => {
+            e.preventDefault;
+            setShowModal(true);
+          }}
+          className="addressBlock addAddress"
+        >
           <Plus />
         </button>
         {authUser.address.map((address) => (
           <AddressBlock key={address._id} address={address} />
         ))}
       </div>
-      <div className="modalContianer">
-        <AddressModal />
-      </div>
+      {showModal && (
+        <div className="modalContianer">
+          <AddressModal closeModal={closeModal} />
+        </div>
+      )}
     </>
   );
 };
